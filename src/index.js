2ee61267b8d3bc918fa70b2b836b8357c6d885f5
Mitch Rhodes <M.Rhodes87@comcast.net>
2021-04-10 18:19:25 -0400

    the board now keeps the state instead of individual squares.

--- src/index.js
+++ src/index.js
@@ -2,30 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
+
+/*The DOM <button> element’s onClick attribute has a special meaning to React
+because it is a built-in component.*/
+
+/*It's best to use on[Event] names for props(properties) for props which 
+represent events and handle[Event] for the methods which handle the events.
+You can give any name to the Square’s onClick prop or Board’s handleClick 
+method, and the code would work the same.*/
+
 class Square extends React.Component {
-    /*In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component 
-    classes that have a constructor should start with a super(props) call.*/
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
+
     render() {
         return (
+            /*The onClick prop on the built-in DOM <button> component tells React to set up a click event listener.
+When the button is clicked, React will call the onClick event handler that is defined in Square’s render() method.
+This event handler calls this.props.onClick(). The Square’s onClick prop was specified by the Board.
+Since the Board passed onClick={() => this.handleClick(i)} to Square, the Square calls this.handleClick(i) when clicked.
+*/
             <button
                 className="square"
-                onClick={() => { this.setState({ value: 'X' }) }}
+                onClick={() => { this.props.onClick({ value: 'X' }) }}
             >
 
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 class Board extends React.Component {
+    /*In JavaScript classes, you need to always call super when defining the constructor of a subclass. All React component 
+    classes that have a constructor should start with a super(props) call.*/
+
+    //the constructor will keep track of the game's state
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({ squares: squares });
+    }
+
+    /*these are what is being passed down to sqaure*/
     renderSquare(i) {
-        return <Square value={i} />;
+        return <Square value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)} />;
     }
 
     render() {
